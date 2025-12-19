75630bd0d515273f04afb842f149b64d0dd7c8db
Fadhiel <fmaulana@student.telkomuniversity.ac.id>
2025-12-19 09:36:50 +0700

    feat: Updated Controller CRUD logic: can now Http Routing

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,47 @@
-import { Controller } from '@nestjs/common';
+import { Controller,
+    Get,
+    Post,
+    Put,
+    Delete,
+    Body,
+    Param,
+    HttpCode,
+    HttpStatus
+ } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto, UpdateUserDto } from './dto/user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly usersService: UsersService) {}
+
+    @Post()
+    @HttpCode(HttpStatus.CREATED)
+    create(@Body() createUserDto: CreateUserDto) {
+        return this.usersService.create(createUserDto);
+    }
+
+    @Get()
+    findAll() {
+        return this.usersService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.usersService.findOne(id);
+    }
+
+    @Put(':id')
+    update(
+        @Param('id') id: string,
+        @Body() updateUserDto: UpdateUserDto
+    ) {
+        return this.usersService.update(id, updateUserDto);
+    }
+
+    @Delete(':id')
+    @HttpCode(HttpStatus.NO_CONTENT)
+    remove(@Param('id') id: string) {
+        return this.usersService.remove(id);
+    }
+}
